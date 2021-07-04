0ebb9de7ac7bad8c327ab5e3f4ccbbf1b98da704
aj2010adil <aj2010adil@gmail.com>
2021-07-04 20:28:41 +0530

    Day 2

--- Array/Hacker_rank_2D_problem.py
+++ Array/Hacker_rank_2D_problem.py
@@ -10,101 +10,39 @@ import sys
 # The function is expected to return an INTEGER.
 # The function accepts 2D_INTEGER_ARRAY arr as parameter.
 #
-def hourglassSum(arr1):
-    # Write your code here
-    '''
-    This is to find the hourglass array
-     abc
-      d
-     efg
-    '''
+arr = [[1,2,3,4     ] ,
+       [5,6,7,8     ], 
+       [9,10,11,12  ],
+       [13,14,15,16]]
+
+def hourglass(arr):
+    k=[]
+    no=0
+    for s in range(len(arr)-2):
+        for d in range( len(arr)-2):
+            no=no+1
+            #print("row={} and col={}  and {} Hourglass".format(s,d,no))             
+            k.append([[arr[s][d],arr[s][d+1],arr[s][d+2]],[0,arr[s+1][d+1],0],[arr[s+2][d],arr[s+2][d]+1,arr[s+2][d+2]]])      
+            
+    return k
+def printHourglass(arr):
+    no=0
+    for k in range(len(arr)):
+        no=no+1
+        print()
+        print("hourglass no {}".format(no))
+        for j in range(len(arr[k])-1):
+            print(hourglass(arr)[k][j])      
         
+    
+    
+def hourglasssum(arr):
+    sum = []
+    for i in range(len(arr)-2):
+        for j in range(len(arr)-2):
+            #print(arr[i][j], end=" ")
+            sum.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+    print(max(sum))
 
 
-
-
-
-#hourglassSum(arr)
-
-T = [[1,2,3,4], [5,6,7,8], [9,10,11,12],[13,14,15,16]]
-#print(T[0][1])
-
-#Starting point of finding the hourglass fo ra given column and row 
-n=4
-#for i in range(0,n):
-#    for k in range(0,n):
-#        print(T[i][k], end=" ")
-#    print()
-#print("End of Array Display")
-#print()
-#
-#for s in range(0,2):
-#    print("{} Hourglass".format(s+1))       
-#    print(T[s+0][0],end = " ")  
-#    print(T[s+0][1],end = " ")  
-#    print(T[s+0][2],end = " ")  
-#    print()
-#    print(end='  ')       
-#    print(T[s+1][1],end = " ")  
-#    print()    
-#    print(T[s+2][0],end = " ")  
-#    print(T[s+2][1],end = " ")  
-#    print(T[s+2][2],end = " ")  
-#    print()
-#
-##column based
-#
-#for s in range(0,2):
-#    print("{} Column Hourglass".format(s+1))       
-#    print(T[0][s+0],end = " ")  
-#    print(T[1][s+0],end = " ")  
-#    print(T[2][s+0],end = " ")  
-#    print()
-#    print(end='  ')       
-#    print(T[1][s+1],end = " ")  
-#    print()    
-#    print(T[0][s+2],end = " ")  
-#    print(T[1][s+2],end = " ")  
-#    print(T[2][s+2],end = " ")  
-#    print()
-
-#automate It
-k=[[1,2,3],[1,2,3]]
-for s in range(0,2):
-    print("{} Hourglass".format(s+1))       
-    k[s+0][0]=T[s+0][0]  
-    k[s+0][1]=T[s+0][1]  
-    k[s+0][2]=T[s+0][2]  
-    k[s+1][1]=T[s+1][1]  
-    k[s+2][0]=T[s+2][0]  
-    k[s+2][1]=T[s+2][1]  
-    k[s+2][2]=T[s+2][2]  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#print(T[0])
-#print(type(T))
-#print(T[1][2])
-#
+printHourglass(arr)
